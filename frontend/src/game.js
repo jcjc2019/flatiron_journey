8dac551bd7852a40ad533bd765c1212af3cea402
github username <github email address>
2019-04-16 14:14:54 -0500

    created player class, bridged player with backend

--- frontend/src/game.js
+++ frontend/src/game.js
@@ -1,45 +1,18 @@
 const divBg = document.querySelector('#bg')
+const playerUrl = 'http://localhost:3000/players/1'
 document.addEventListener("DOMContentLoaded", function(){
    //fetch player 
-   const player =  document.createElement('img')
-   divBg.appendChild(player)
-   player.src = 'assets/test.gif'
-   player.style.width = '300px'
-   player.style.height = '300px'
-   player.style.left = "20px"
-   player.style.bottom = "0px"
-   player.style.position = 'absolute'
-   console.log('you reached me')
+   fetch(playerUrl)
+   .then(function(response){
+     return response.json()
+   })
+   .then(function(playerData){
+    //instantiate player 
+    new Player(playerData)
+    console.log(playerData) 
+   })
+   
 
    
-  document.addEventListener('keydown', e=> {
-    divBg.className = "animation"
-    var dir = null
-    if(e.key == "ArrowRight") {
-      dir = 'right'
-    }
-    else if(e.key == "ArrowLeft") {
-      dir = 'left'
-    }
-    startMoving(dir)
-  })
-
-  function startMoving(direction) {
-    console.log(divBg.style)
-    let left = parseInt(player.style.left)
-    let bgpos = divBg.style.backgroundPositionX
-    if(direction === 'right') {
-      player.style.left = `${left + 50}px`
-      divBg.style.backgroundPositionX = `${bgpos + 10}px`
-    }
-    else if(direction === 'left') {
-      player.style.left = `${left - 50}px`
-      divBg.style.backgroundPositionX = `${bgpos - 10}px`
-    }
-  }
-
-
-
-
-   
-})
+  
+}) 
--- frontend/src/playerMovement.js
+++ frontend/src/playerMovement.js
@@ -1,3 +1,15 @@
-document.addEventListener("DOMContentLoaded", function(){
-    
-})
+
+  function startMoving(dir,player) {
+    console.log(divBg.style)
+    console.log(player.x)
+    let left = parseInt(player.element.style.left)
+    let bgpos = divBg.style.backgroundPositionX
+    if(dir === 'right') {
+      player.element.style.left = `${left + 50}px`
+      divBg.style.backgroundPositionX = `${bgpos + 10}px`
+    }
+    else if(dir === 'left') {
+      player.element.style.left = `${left - 50}px`
+      divBg.style.backgroundPositionX = `${bgpos - 10}px`
+    }
+  }
