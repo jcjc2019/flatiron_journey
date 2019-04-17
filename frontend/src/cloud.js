7b1e396aaed930a31710481bb29bde1550fcf6cc
JingChen <jingchenjc2019@gmail.com>
2019-04-17 09:05:55 -0500

    star index added.

--- frontend/src/cloud.js
+++ frontend/src/cloud.js
@@ -8,10 +8,6 @@ document.addEventListener("DOMContentLoaded", function(event){
         "assets/cloud_2.png",
         { name: "loop2", url: "assets/loop2.mp3" }
     ],).load(init)
-        //    const sound = PIXI.sound.Sound.from('assets/loop2.mp3');
-        //    sound.play();
-        // var loop2 = PIXI.audioManager.getAudio('loop2');
-        // loop2.play();
 
     function init() {
         renderer.backgroundColor = 0x22A7F0;
