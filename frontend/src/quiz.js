863ac5e3a6a5e94a5b27ce55078e4deb424e4206
JingChen <jingchenjc2019@gmail.com>
2019-04-18 15:41:43 -0500

    starting quiz part.

--- frontend/src/quiz.js
+++ frontend/src/quiz.js
@@ -1,4 +1,81 @@
-function buildQuiz() { }
+
+const dunDiv = document.querySelector("#dungeon")
+const quizContainer = document.createElement('div')
+quizContainer.setAttribute("id", "quiz")
+
+const submitButton = document.createElement('button')
+submitButton.setAttribute('id', 'submit')
+
+const resultsContainer = document.createElement('div')
+resultsContainer.setAttribute('id', 'results')
+dunDiv.append(quizContainer, submitButton, resultsContainer)
+
+const myQuestions = [
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: ""
+        },
+        correctAnswer: ""
+    }
+]
+
+const mod2Question = [
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: ""
+        },
+        correctAnswer: ""
+    }
+]
+
+const mod3Question = [
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: ""
+        },
+        correctAnswer: ""
+    }
+]
+
+const mod4Question = [
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: ""
+        },
+        correctAnswer: ""
+    }
+]
+
+const mod5Question = [
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: ""
+        },
+        correctAnswer: ""
+    }
+]
+
+function buildQuiz() { 
+    const output = [];
+    mod1Question.forEach((currentQuestion, questionnumber) =>{
+        const answers = [];
+    })
+}
 
 function showResults() { }
 
