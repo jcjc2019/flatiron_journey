35e4070efc29cfebebe8e031b164592bc4bd2f6e
sm00th <ezzybrzy@sm00ths-MacBook-Air.local>
2019-04-18 15:38:07 -0500

    begin refactor

--- frontend/src/player.js
+++ frontend/src/player.js
@@ -35,7 +35,19 @@ class Player{
            console.log(player.style.left)
            console.log(window.scrollX)
            // spaghetti code to render the dugeon
-           if(window.scrollX < 900 && window.scrollX > 720) {
+
+            if(window.scrollX < 900 && window.scrollX > 720) {
+              
+            }
+            else if(window.scroll ) {
+
+            }
+
+            function show() {
+              dunDiv.style.opacity = (parseFloat(dunDiv.style.opacity) + 0.3).toString()
+            }
+            setInterval(show, 300)
+            /*
               let dunDiv = document.createElement('div')
               dunDiv.id = "dungeon"
               let monsterOne = purpleMonster
@@ -44,12 +56,14 @@ class Player{
               dunDiv.style.backgroundSize = "2000px 352px"
               dunDiv.style.opacity = 0
               /*______MONSTER______*/
+            /*
               monsterOne.style = 'box-sizing: content-box'
               monsterOne.style.position = 'absolute'
               monsterOne.style.top = '150px'
               monsterOne.style.left = '60px'
               // dunDiv.style.left = '150px'
               /*______DUNGEON______*/
+            /*
               dunDiv.style.top = '443px'
               dunDiv.style.bottom = '200px'
               dunDiv.style.height = '352px'
@@ -58,16 +72,8 @@ class Player{
               dunDiv.append(player)
               dunDiv.append(monsterOne)
               document.body.append(dunDiv)
-              
-              function show() {
-                dunDiv.style.opacity = (parseFloat(dunDiv.style.opacity) + 0.3).toString()
-                console.log(dunDiv.style.opacity)
-              }
-              setInterval(show, 300)
-              
-
+            */
            } // end of first stage
-
 
             if(e.repeat) return 
               //bg.className = "animation"
@@ -81,6 +87,32 @@ class Player{
             }
           })
     }
+
+    renderDungeon() {
+      let dunDiv = document.createElement('div')
+      dunDiv.id = "dungeon"
+      let monsterOne = purpleMonster
+      dunDiv.style.backgroundImage ="url('https://img.itch.zone/aW1hZ2UvMjI0OTk4LzEwNjMyOTAucG5n/original/p3QHJw.png')"
+      dunDiv.style.backgroundRepeat = "no-repeat"
+      dunDiv.style.backgroundSize = "2000px 352px"
+      dunDiv.style.opacity = 0
+      /*______MONSTER______*/
+      monsterOne.style = 'box-sizing: content-box'
+      monsterOne.style.position = 'absolute'
+      monsterOne.style.top = '150px'
+      monsterOne.style.left = '60px'
+      // dunDiv.style.left = '150px'
+      /*______DUNGEON______*/
+      dunDiv.style.top = '443px'
+      dunDiv.style.bottom = '200px'
+      dunDiv.style.height = '352px'
+      dunDiv.style.width = '100%'
+      dunDiv.style.position = 'fixed'
+      dunDiv.append(player)
+      dunDiv.append(monsterOne)
+      document.body.append(dunDiv)
+    }
+
 
     stop=(player, playerData)=>{
         document.addEventListener('keyup', e=> {
