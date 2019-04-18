912771d64b4647fc76ff66cb656c9da85024f3a8
JingChen <jingchenjc2019@gmail.com>
2019-04-18 14:00:18 -0500

    unnecessary files deleted. form events to be fixed.

--- frontend/src/game-data.js
+++ frontend/src/game-data.js
@@ -1,37 +1,46 @@
-//variables for forms
-const newGameForm = document.querySelector("#new-game-form")
-const resumeGameForm = document.querySelector("#resume-game-form")
 
-//set up const for input value
-const newName = document.querySelector("#input-username")
-const newImg = document.querySelector("input[name=character]:checked")
-
-const oldName = document.querySelector("#input-old-username")
-
-const playersURL = "http://localhost:3000/players"
-
-//1. submit new form action
-// post player information to database, create new player instance
-const createNewPlayerImg = function () {
-  //patch
-  fetch(playersURL,{
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      username: newName.value,
-      staticImgUrl: `assets/${newImg.value}_static.gif`,
-      leftImgUrl: `assets/${newImg.value}_left.gif`,
-      rightImgUrl: `assets/${newImg.value}_right.gif`,
-      score: 0,
-      x: 20
-    })
-    // .then((res) => res.json())
-    // .then((newPlayerData) => new Player(newPlayerData))
-  })
-}
 //add event listener to newGameForm
-newGameForm.addEventListener('submit', function(){
-  createNewPlayerImg()
+document.addEventListener("DOMContentLoaded", ()=>{
+
+
+  const newGameForm = document.getElementById("new-game-form")
+  const resumeGameForm = document.querySelector("#resume-game-form")
+
+  //set up const for input value
+  const newName = document.querySelector("#input-username")
+  const newImg = document.querySelector("input[name=character]:checked")
+  const oldName = document.querySelector("#input-old-username")
+
+  const playersURL = "http://localhost:3000/players"
+
+  //1. submit new form action
+  // post player information to database, create new player instance
+  const createNewPlayerImg = function () {
+    //patch
+    fetch(playersURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: newName.value,
+        staticImgUrl: `assets/${newImg.value}_static.gif`,
+        leftImgUrl: `assets/${newImg.value}_left.gif`,
+        rightImgUrl: `assets/${newImg.value}_right.gif`,
+        score: 0,
+        x: 20
+      })
+      // .then((res) => res.json())
+      // .then((newPlayerData) => new Player(newPlayerData))
+    })
+  }
+
+  //add event for form
+  newGameForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    createNewPlayerImg();
+  })
+
 })
+
+
