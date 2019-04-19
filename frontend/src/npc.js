0e120e53e6693678c2b8e9b7952860e69f9f87b4
JingChen <jingchenjc2019@gmail.com>
2019-04-19 06:01:45 -0500

    forms fixed.

--- frontend/src/npc.js
+++ frontend/src/npc.js
@@ -1,6 +1,7 @@
 
 function talkToJosh(joshConvo){
    const p = document.createElement('p')
+   p.id = "conversation"
    p.style.marginLeft = "250px"
    p.style.top = "10px"
    p.innerText = "Good morning loves! We'll start our journey to programming world now! Mod1 is OOP Ruby. Let's use the superhero and hero abilities as example...."
@@ -14,6 +15,7 @@ function talkToJosh(joshConvo){
 
 function talkToMonsterLight(monsterLightConvo){
    const p = document.createElement('p')
+   p.id = "conversation"
    p.style.marginLeft = "1180px"
    p.style.top = "20px"
    p.innerText = "I am the Mod 1 challenge! How is Ruby treating you so far? What? you want to continue your journey? Hold on hold on, pass me first!"
@@ -27,6 +29,7 @@ function talkToMonsterLight(monsterLightConvo){
 
 function talkToPurpleMonster(purpleMonsterConvo){
    const p = document.createElement('p')
+   p.id = "conversation"
    p.style.marginLeft = "2010px"
    p.style.top = "20px"
    p.innerText = "Baaaaaang! I am the Mod 2 challenge! Are you getting along with Rails? ActiveRecord is awesome but you have to understand how SQL works. So, tell me what is 'CRUD'?"
@@ -39,6 +42,7 @@ function talkToPurpleMonster(purpleMonsterConvo){
 
 function talkToDragon(dragonConvo){
    const p = document.createElement('p')
+   p.id = "conversation"
    p.style.marginLeft = "3400px"
    p.style.top = "180px"
    p.innerText = "Hey you stop here! Yeah it'd you, don't look at others. Are you ready for the Mod 3 challenge! Show me how to fetch the info from server.... or POST? PATCH?? Oh I forget about the headers...Urgh... "
@@ -52,6 +56,7 @@ function talkToDragon(dragonConvo){
 
 function talkToRedDragon(redDragonConvo){
    const p = document.createElement('p')
+   p.id = "conversation"
    p.style.marginLeft = "4750px"
    p.style.top = "40px"
    p.innerText = "Impressed! I didn't expect you can last this long. Now we are here in Mod 5, get ready for the challenge! Show me how to grab dynamic user info using React. No no, you don't hardcode it, I mean DRY. Gosh why you can't remember DRY principle...."
@@ -64,6 +69,7 @@ function talkToRedDragon(redDragonConvo){
 
 function talkToFinal(finalConvo){
    const p = document.createElement('p')
+   p.id = "conversation"
    p.style.marginLeft = "6100px"
    p.style.top = "40px"
    p.innerText = "Muahahahahahahah...I am the final project! Now is the time to shine (or die). Let's put it all together, let me see if you are qualified to graduate. Josh, what do you think of this project?"
--- frontend/src/set-stage-size.js
+++
@@ -1,4 +0,0 @@
-const gameStage = document.querySelector("#stage");
-
-gameStage.style.width = window.innerWidth + "px";
-gameStage.style.height = window.innerHeight + "px";
